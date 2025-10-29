baaef26cc07f169de5962af82c15b945bb8bc91b
Alessandro Venturini <ales.ventus21@gmail.com>
2025-10-29 11:55:03 +0100

    parametrized script
    parametrized the script for an input directory and an output directory
    also added flags for:
    - generating only staged changes
    - adding generated diagrams to the stage

--- gen-puml-diagrams.sh
+++ gen-puml-diagrams.sh
@@ -6,47 +6,81 @@
 # Redirect output to stderr.
 exec 1>&2
 
+set -eu
+
+show_help() {
+	echo "Usage: $0 [-o] <puml sources folder> <output folder>"
+	echo "       -o to generate diagrams only for staged changes"
+	echo "       -s to automatically stage all generated diagrams"
+}
+
+ONLY_STAGED=false
+STAGE_GENERATED_DIAGRAMS=false
+
+while getopts 'hos' opt; do
+	case $opt in
+	h)
+		show_help
+		exit
+		;;
+	o) ONLY_STAGED=true ;;
+	s) STAGE_GENERATED_DIAGRAMS=true ;;
+	esac
+done
+shift "$((OPTIND - 1))"
+
+if [ $# -lt 2 ]; then
+	show_help
+	exit 1
+fi
+
+IN_DIR="$1"
+OUT_DIR="$2"
+
 if ! which docker >&/dev/null; then
 	echo "Docker is required for generating PlantUML diagrams"
 	exit 1
 fi
 
-PROJECT_ROOT="$(pwd)/$(dirname $0)/../.."
-DIAGRAMS_DIR="${PROJECT_ROOT}/doc/diagrams"
+# Checks if there are any changes in the sources or in the generated diagrams
+# If ONLY_STAGED is set we check only for staged changes
+if ! git diff --quiet $($ONLY_STAGED && echo --staged) -- "$IN_DIR" "$OUT_DIR"; then
 
-# Checks if there are any staged changes in the diagrams
-if ! git diff --quiet --staged -- "$DIAGRAMS_DIR"; then
-
-	# Stashing everything that is not staged (so we can build images only of what will be committed)
-	git stash push --quiet --keep-index --include-untracked
-
-	# Copying the diagrams folder into a temporary one
-	TEMP_DIR=$(mktemp -d)
-	cp -r "$DIAGRAMS_DIR/"* "$TEMP_DIR"
-
-	# Resetting the repo in order to avoid merge conflicts when popping the stash
-	git reset --quiet --hard
-	# Removing untracked files as this is not done by git reset (they will be restored when popping the stash)
-	git clean --quiet -fd
-
-	git stash pop --quiet --index
-
-	# From now on we can safely work on the temp folder
-
-	# Removing all the diagrams so that if a src was deleted the generated diagram will be deleted as well
-	rm -f "$TEMP_DIR/generated/"*
-
+	# Pulling the image before doing anything in so that we do nothing if the pull fails
 	echo "Pulling plantuml image"
 	docker pull plantuml/plantuml:latest >&/dev/null
 
-	echo "Generating UML diagrams"
-	if docker run --rm -v /${TEMP_DIR}:/data plantuml/plantuml:latest -failfast2 -o ../generated //data/src; then
-		# Substituting the generated folder with the new one
-		rm -rf "$DIAGRAMS_DIR/generated"
-		cp -r "$TEMP_DIR/generated" "$DIAGRAMS_DIR/generated"
+	if $ONLY_STAGED; then
+		# Stashing everything that is not staged (so we can build images only of what will be committed)
+		git stash push --quiet --keep-index --include-untracked
+		# Resetting the repo in order to avoid merge conflicts when popping the stash
+		git reset --quiet --hard
+		# Removing untracked files as this is not done by git reset (they will be restored when popping the stash)
+		git clean --quiet -fd
+		# Now we have just the staged changes
+	fi
 
-		# Staging the generated diagrams in order to commit them
-		git add "$DIAGRAMS_DIR/generated"
+	# Copying the sources folder into a temporary one
+	TEMP_DIR=$(mktemp -d)
+	TEMP_SRC="$TEMP_DIR/src"
+	TEMP_GEN="$TEMP_DIR/gen"
+	cp -r "$IN_DIR/"* "$TEMP_SRC"
+
+	if $ONLY_STAGED; then
+		# Here we pop while also restoring the index because from now on we can work on the temp folder
+		git stash pop --quiet --index
+	fi
+
+	echo "Generating UML diagrams"
+	if docker run --rm -v /${TEMP_SRC}:/data plantuml/plantuml:latest -failfast2 -o ../gen //data/src; then
+		# Substituting the generated folder with the new one
+		rm -rf "$OUT_DIR"
+		cp -r "$TEMP_GEN" "$OUT_DIR"
+
+		if $STAGE_GENERATED_DIAGRAMS; then
+			# Staging the generated diagrams in order to commit them
+			git add "$OUT_DIR"
+		fi
 
 		# Remove temporary folder
 		rm -rf "$TEMP_DIR"
