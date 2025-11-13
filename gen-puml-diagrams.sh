e575715bc1775f6e5729f9925d196de2719868b9
Alessandro Venturini <ales.ventus21@gmail.com>
2025-11-13 21:39:25 +0100

    script can load in and out dirs from config file
    This allows the user to write a configuration file which will be the only place where he
    defines input and output directories.
    In this way there will be no need to repeat them when:
    - running the script manually
    - invoking the script via git hook
    - invoking the script via GitHub action

--- gen-puml-diagrams.sh
+++ gen-puml-diagrams.sh
@@ -5,44 +5,72 @@ exec 1>&2
 
 set -eu
 
+DEFAULT_CONFIG_FILE_NAME="gen-puml-diagrams-config.sh"
+
 usage() {
     BASENAME=$(basename "$0")
     cat <<EOF
-Usage: $BASENAME [-h] [-p] <input-dir> <output-dir>
+Usage:
+    1) $BASENAME [-h] [-p] [<input-dir> <output-dir>]
+    2) $BASENAME [-h] [-p] [-c <config-file>]
 
-Generate PlantUML diagrams contained in <input-dir> into
-<output-dir> mirroring the folders structure.
-<input-dir> and <output-dir> are directly passed to PlantUML so if you want to
-understand better how it works you should read the PlantUML CLI documentation.
+Description:
+    Generate PlantUML diagrams from sources contained in <input-dir> into
+    <output-dir> mirroring the directory structure.
+    <input-dir> and <output-dir> are directly passed to PlantUML so if you want to
+    understand better how they work you should read the PlantUML CLI documentation.
 
+Configuration file:
+    When used in form number 2 the script will source a configuration file to get
+    the input and output directories.
+    The configuration file can be specified by the -c option, otherwise a file
+    named "$DEFAULT_CONFIG_FILE_NAME" will be recursively searched upward from
+    this script location.
+    The configuration file content should be something like the following:
+
+        INPUT_DIR=diagrams/src
+        OUTPUT_DIR=diagrams/gen
+  
 Options:
-  -h  Show this message and exit
-  -p  (pre-commit) The script will generate diagrams reflecting only the staged
-      changes and adding the generated diagrams to the stage. It is meant to be
-      run as a pre-commit git hook
+    -h
+        Show this message and exit
+
+    -p  
+        (pre-commit) The script will generate diagrams reflecting only the staged
+        changes and adding the generated diagrams to the stage. It is meant to be
+        run as a pre-commit git hook
+
+    -c  <config-file> 
+        Path to the configuration file
 
 Examples:
-  $BASENAME diagrams/src diagrams/gen
-  $BASENAME -p diagrams/src diagrams/gen
+    $BASENAME diagrams/src diagrams/gen
+    $BASENAME -p diagrams/src diagrams/gen
+    $BASENAME 
+    $BASENAME -p -c config.sh
 EOF
 }
 
 PRE_COMMIT=false
+CONFIG_FILE=""
+INPUT_DIR=""
+OUTPUT_DIR=""
 
-while getopts ':hp' opt; do
+while getopts ':hpc:' opt; do
     case $opt in
     h)
         usage
         exit
         ;;
     p) PRE_COMMIT=true ;;
+    c) CONFIG_FILE="$OPTARG" ;;
     :)
-        echo "Error: Option -$OPTARG requires an argument."
+        echo "Error: Option -$OPTARG requires an argument." >&2
         usage
         exit 1
         ;;
     \?)
-        echo "Error: Invalid option -$OPTARG"
+        echo "Error: Invalid option -$OPTARG" >&2
         usage
         exit 1
         ;;
@@ -50,21 +78,57 @@ while getopts ':hp' opt; do
 done
 shift "$((OPTIND - 1))"
 
-if [ $# -lt 2 ]; then
+if [ $# -eq 2 ]; then
+    if [ -z "$CONFIG_FILE" ]; then
+        INPUT_DIR="$1"
+        OUTPUT_DIR="$2"
+    else
+        usage
+        exit 1
+    fi
+else
+    # Searching for the config file going upward in the file tree
+    # if it is not already specified
+    DIR=$(
+        cd "$(dirname "$0")"
+        pwd
+    )
+    while [ -z "$CONFIG_FILE" ] && [ "$DIR" != "/" ]; do
+        FILE="$DIR/$DEFAULT_CONFIG_FILE_NAME"
+        if [ -f "$FILE" ]; then
+            CONFIG_FILE="$FILE"
+            break
+        fi
+        DIR=$(dirname "$DIR")
+    done
+    unset DIR
+    unset FILE
+
+    if [ ! -r "$CONFIG_FILE" ]; then
+        echo "Error: configuration file not found or not readable" >&2
+        exit 1
+    fi
+
+    echo "Sourcing configuration file" >&2
+    # shellcheck disable=SC1090
+    . "$CONFIG_FILE"
+fi
+
+if [ ! -d "$INPUT_DIR" ] || [ ! -d "$OUTPUT_DIR" ]; then
+    echo "Error: invalid input directory ($INPUT_DIR) or output directory ($OUTPUT_DIR)" >&2
     usage
     exit 1
 fi
 
-IN_DIR="$1"
-OUT_DIR="$2"
-
 if ! command -v docker >/dev/null 2>&1; then
-    echo "Docker is required for generating PlantUML diagrams"
+    echo "Error: Docker is required for generating PlantUML diagrams" >&2
     exit 1
 fi
 
+# --------- END OF VALIDATION ---------
+
 # If PRE_COMMIT is set we check if there are staged changes
-if ! $PRE_COMMIT || ! git diff --quiet --staged -- "$IN_DIR" "$OUT_DIR"; then
+if ! $PRE_COMMIT || ! git diff --quiet --staged -- "$INPUT_DIR" "$OUTPUT_DIR"; then
 
     # Pulling the image before doing anything in so that we do nothing if the pull fails
     echo "Pulling plantuml Docker image..."
@@ -82,7 +146,7 @@ if ! $PRE_COMMIT || ! git diff --quiet --staged -- "$IN_DIR" "$OUT_DIR"; then
     mkdir "$TEMP_SRC"
     TEMP_GEN="$TEMP_DIR/gen"
     mkdir "$TEMP_GEN"
-    cp -r "$IN_DIR/"* "$TEMP_SRC" || true # TODO: should we suppress stderr?
+    cp -r "$INPUT_DIR/"* "$TEMP_SRC" || true # TODO: should we suppress stderr?
 
     if $PRE_COMMIT; then
         # Resetting the repo in order to avoid merge conflicts when popping the stash
@@ -94,12 +158,12 @@ if ! $PRE_COMMIT || ! git diff --quiet --staged -- "$IN_DIR" "$OUT_DIR"; then
     echo "Generating UML diagrams"
     if docker run --rm -v "/${TEMP_DIR}":/data plantuml/plantuml:latest -failfast2 -o ../gen //data/src; then
         # Substituting the generated folder with the new one
-        rm -rf "$OUT_DIR"
-        cp -r "$TEMP_GEN" "$OUT_DIR"
+        rm -rf "$OUTPUT_DIR"
+        cp -r "$TEMP_GEN" "$OUTPUT_DIR"
 
         if $PRE_COMMIT; then
             # Staging the generated diagrams
-            git add "$OUT_DIR"
+            git add "$OUTPUT_DIR"
         fi
 
         # Remove temporary folder
