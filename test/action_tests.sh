e811d1264f39fd994b5ed24a1ed22179d071f8bc
Alessandro Venturini <ales.ventus21@gmail.com>
2025-11-20 22:57:00 +0100

    test: refactor action_tests.sh

--- test/action_tests.sh
+++ test/action_tests.sh
@@ -16,62 +16,69 @@ cleanup() {
 }
 trap cleanup EXIT
 
-# ~~~~~~~~~~ TEST 1 ~~~~~~~~~~
+# Takes one parameter, executes it as a command and tracks if it
+# succeds or fails, generating a report.
+# You should define your test in a function and then pass it to run_test.
+# run_test will automatically cd in a clean dir on every invocation.
+run_test() {
+    TEST="$1"
 
-mkdir "$TEST_DIR"
-cd "$TEST_DIR"
-git init
-# making initial commit
-git commit --allow-empty --allow-empty-message -m ""
-BASE_COMMIT=$(git log -1 --format="%H")
-mkdir src
-mkdir gen
-cat >src/d.puml <<EOF
+    mkdir "$TEST_DIR"
+    cd "$TEST_DIR"
+
+    echo "Running $TEST ..."
+    if $TEST >/dev/null 2>&1; then
+        printf "\033[1A\033[2K"
+        echo "✅ $TEST"
+        PASSED="$PASSED
+        $TEST"
+    else
+        printf "\033[1A\033[2K"
+        echo "❌ $TEST"
+        FAILED="$FAILED
+        $TEST"
+    fi
+    cleanup
+}
+
+action_fails_if_diagrams_not_generated_correctly_on_first_commit() {
+    git init
+    # making initial commit
+    git commit --allow-empty --allow-empty-message -m ""
+    BASE_COMMIT=$(git log -1 --format="%H")
+    mkdir src
+    mkdir gen
+    cat >src/d.puml <<EOF
 @startuml
 Bob -> Alice : hello
 @enduml
 EOF
-git add .
-git commit --allow-empty-message -m ""
+    git add .
+    git commit --allow-empty-message -m ""
 
-if ../../action.sh -f "$BASE_COMMIT" HEAD "../../gen_puml_diagrams.sh src gen"; then
-    echo "Test 1 not passed"
-    FAILED="${FAILED}Test1 "
-else
-    echo "Test 1 passed"
-    PASSED="${PASSED}Test1 "
-fi
-cd ..
-rm -rf "$TEST_DIR"
+    ! ../../action.sh -f "$BASE_COMMIT" HEAD "../../gen_puml_diagrams.sh src gen"
+}
+run_test action_fails_if_diagrams_not_generated_correctly_on_first_commit
 
-# ~~~~~~~~~~ TEST 2 ~~~~~~~~~~
-
-mkdir "$TEST_DIR"
-cd "$TEST_DIR"
-git init
-# making initial commit
-git commit --allow-empty --allow-empty-message -m ""
-BASE_COMMIT=$(git log -1 --format="%H")
-mkdir src
-mkdir gen
-cat >src/d.puml <<EOF
+action_succeed_if_diagrams_are_generated_correctly_on_first_commit() {
+    git init
+    # making initial commit
+    git commit --allow-empty --allow-empty-message -m ""
+    BASE_COMMIT=$(git log -1 --format="%H")
+    mkdir src
+    mkdir gen
+    cat >src/d.puml <<EOF
 @startuml
 Bob -> Alice : hello
 @enduml
 EOF
-../../gen_puml_diagrams.sh src gen
-git add .
-git commit --allow-empty-message -m ""
+    ../../gen_puml_diagrams.sh src gen
+    git add .
+    git commit --allow-empty-message -m ""
 
-if ../../action.sh -f "$BASE_COMMIT" HEAD "../../gen_puml_diagrams.sh src gen"; then
-    echo "Test 2 passed"
-    PASSED="${PASSED}Test2 "
-else
-    echo "Test 2 not passed"
-    FAILED="${FAILED}Test2 "
-fi
-cd ..
-rm -rf "$TEST_DIR"
+    ../../action.sh -f "$BASE_COMMIT" HEAD "../../gen_puml_diagrams.sh src gen"
+}
+run_test action_succeed_if_diagrams_are_generated_correctly_on_first_commit
 
 echo PASSED:
 for T in $PASSED; do
